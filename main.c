f0fa219053780377ad3c588e98e393a94c7f01a7
20161104589 <915327031@qq.com>
2017-06-13 10:55:34 +0800

    将排序写入文件中

--- main.c
+++ main.c
@@ -1,32 +1,50 @@
-//
-//  main.c
-//  rank
-//
-//  Created by 杨志伟 on 2017/6/7.
-//  Copyright © 2017年 杨志伟. All rights reserved.
-//
 
 #include <stdio.h>
 
-int main()
+int main(int argc,const char *argv[])
 {
-   
-    int i,j,temp;
+    FILE *fp1,*fp2;
+    int i,j,n,temp;
     int  a[10];
-   
-    for(i=1;i<=10;i++)
+    fp1=fopen("//Users//a20161104589//Desktop//test//rank//20161104589//rank//wirteout.txt","w");
+    fp2=fopen("//Users//a20161104589//Desktop//test//rank//20161104589//rank//write.txt","w");
+    if(fp2==NULL)
     {
-        scanf("%d ",&a[i]);
+        printf("打开文件错误，要打开的文件可能不存在");
     }
-    for(i=1;i<=9;i++)
-    for(j=1;j<11-i;j++)
-    if(a[j]>a[j+1])
+    else
     {
-        temp=a[j];
-        a[j]=a[j+1];
-        a[j+1]=temp;
+       while(fscanf(fp2,"%d",&n)!=EOF)
+       {
+           for(i=1;i<=10;i++)
+          {
+              fscanf(fp2,"%d",&a[i]);
+          }
+          for(i=1;i<=9;i++)
+         {
+            for(j=1;j<11-i;j++)
+            {
+               if(a[j]>a[j+1])
+               {
+                  temp=a[j];
+                  a[j]=a[j+1];
+                  a[j+1]=temp;
+               }
+            }
+
+        }
+        for(i=1;i<=10;i++)
+        {
+            printf("%d ",a[i]);
+            fprintf(fp1,"%d",a[i]);
+        }
+           fprintf(fp1,"\n");
+           printf("\n");
+
+       }
+        fclose(fp1);
+        fclose(fp2);
     }
-    for(i=1;i<=10;i++)
-        printf("%d\n ",a[i]);
+
     return 0;
 }
